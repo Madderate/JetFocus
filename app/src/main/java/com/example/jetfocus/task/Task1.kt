da991a033523f4d8d35ecc62d41351d7cec1446b
madderate <jianzengzhi.liao@duitang.com>
2023-07-15 20:23:51 +0800

    Task 1 answer is completed.

--- app/src/main/java/com/example/jetfocus/task/Task1.kt
+++ app/src/main/java/com/example/jetfocus/task/Task1.kt
@@ -1,16 +1,17 @@
 package com.example.jetfocus.task
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
-/* 写出你在这次活动的第一个Composable函数，写完以后删除注释可以看到结果 */
-class Task1 {
+// 写一个HelloWorld的Composable函数并绘制一个“Hello Jetpack Compose!”的文字。
+@Composable
+fun HelloWorld() {
+    Text("Hello World")
+}
 
-    // TODO(写一个HelloWorld的Composable函数并绘制一个“Hello Jetpack Compose!”的文字)
-
-    @Preview
-    @Composable
-    fun Preview() {
-        // HelloWord()
-    }
+@Composable
+@Preview()
+fun Preview() {
+    HelloWorld()
 }
