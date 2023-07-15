69c92b289e82f9591647ac5b16668427886d63a8
madderate <jianzengzhi.liao@duitang.com>
2023-07-15 20:28:59 +0800

    Task 3 answer is completed.

--- app/src/main/java/com/example/jetfocus/task/Task3.kt
+++ app/src/main/java/com/example/jetfocus/task/Task3.kt
@@ -1,33 +1,31 @@
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
 
-/*熟悉布局：Row。*/
 class Task3 {
-    /*
-    * TODO(1. 在现在的基础上，往图片右方加两个文字；一个显示用户工作另一个显示简介)
-    * TODO(2. 用户工作为："Android Developer", 简介是："Andy 是相当有经验的安卓开发者，他参与了Google的各类大型项目，他喜欢游泳和烘培。")
-    */
-
+    // 改变图像的大小到96dp
     @Composable
-    fun UserInfoCard() {
+    fun Card() {
         Column {
             Image(
-                painter = painterResource(R.drawable.andy),
+                modifier = Modifier.size(96.dp),
+                painter = painterResource(R.drawable.jetpack_compose),
                 contentDescription = null
             )
-            Text("Andy Holloway")
+            Text("Hello World")
         }
     }
 
     @Composable
-    @Preview
+    @Preview()
     fun Preview() {
-        UserInfoCard()
+        Card()
     }
 }
-
