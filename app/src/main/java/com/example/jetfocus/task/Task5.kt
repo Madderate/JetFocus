5cbaf445b51c0cdc9f65a6e59174fb3abe3d8553
madderate <jianzengzhi.liao@duitang.com>
2023-07-15 20:32:19 +0800

    Task5 is answer is completed.

--- app/src/main/java/com/example/jetfocus/task/Task5.kt
+++ app/src/main/java/com/example/jetfocus/task/Task5.kt
@@ -1,15 +1,11 @@
 package com.example.jetfocus.task
 
-import android.content.res.Configuration
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,38 +17,29 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
 
-// 主题（皮肤或Theming）
 class Task5 {
-    // TODO("把Preview的UserInfoCard视图包含在TaskTheme和Surface里面然后观察他们的变化。（尝试把Surface去掉）")
+    // 把图片圆边和为个视图加一下空间
     @Composable
-    fun UserInfoCard() {
-        Row(modifier = Modifier.fillMaxWidth()) {
-            Column(
-                modifier = Modifier.padding(12.dp),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Image(
-                    modifier = Modifier
-                        .size(56.dp)
-                        .clip(CircleShape),
-                    painter = painterResource(R.drawable.andy),
-                    contentDescription = null
-                )
-                Spacer(modifier = Modifier.height(8.dp))
-                Text("Andy Holloway")
-            }
-            Column(modifier = Modifier.padding(vertical = 12.dp)) {
-                Text("Android Developer")
-                Spacer(modifier = Modifier.height(10.dp))
-                Text("Andy 是相当有经验的安卓开发者，他参与了Google的各类大型项目，他喜欢游泳和烘培。")
-            }
+    fun Card() {
+        Column(
+            modifier = Modifier.padding(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier
+                    .size(96.dp)
+                    .clip(CircleShape),
+                painter = painterResource(R.drawable.jetpack_compose),
+                contentDescription = null
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text("Hello World")
         }
     }
 
     @Composable
-    @Preview(showSystemUi = true)
-    @Preview(showSystemUi = true, uiMode = Configuration.UI_MODE_NIGHT_YES)
+    @Preview()
     fun Preview() {
-        UserInfoCard()
+        Card()
     }
 }
