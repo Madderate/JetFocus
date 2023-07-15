326581ad980d0aecc8b72385564c2d1cb7db8335
madderate <jianzengzhi.liao@duitang.com>
2023-07-15 20:30:58 +0800

    Task 4 answer is completed.

--- app/src/main/java/com/example/jetfocus/task/Task4.kt
+++ app/src/main/java/com/example/jetfocus/task/Task4.kt
@@ -2,37 +2,37 @@ package com.example.jetfocus.task
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.jetfocus.R
 
-// Modifiers。
 class Task4 {
-    // TODO(调整各个视图的大小，对齐，左右上下空间使界面更加整齐，然后把图片改成圆角。)
+    // 把视图水平对齐
     @Composable
-    fun UserInfoCard() {
-        Row {
-            Column {
-                Image(
-                    painter = painterResource(R.drawable.andy), contentDescription = null
-                )
-                Text("Andy Holloway")
-            }
-            Column {
-                Text("Android Developer")
-                Text("Andy 是相当有经验的安卓开发者，他参与了Google的各类大型项目，他喜欢游泳和烘培。")
-            }
+    fun Card() {
+        Column(
+             horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Image(
+                modifier = Modifier.size(96.dp),
+                painter = painterResource(R.drawable.jetpack_compose),
+                contentDescription = null
+            )
+            Text(
+                text = "Hello World",
+            )
         }
     }
 
     @Composable
-    @Preview
+    @Preview()
     fun Preview() {
-        UserInfoCard()
+        Card()
     }
 }
-
-
